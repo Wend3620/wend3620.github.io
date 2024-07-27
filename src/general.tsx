5a7af1058bec2a381bbc96369bbf0a1ebb75382f
WENYUAN <wdu36@wisc.edu>
2024-07-27 15:21:57 +0800

    different size accomendation

--- src/general.tsx
+++ src/general.tsx
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import {Nav, NavDropdown} from 'react-bootstrap';  
 import './decor/helper.css';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import { motion} from "framer-motion";
+import {useMediaQuery} from 'react-responsive'
 
 
 export interface GeneralProps {
@@ -17,12 +18,17 @@ export default function General({currentPage}: {readonly currentPage:string}){
   // }else{
   //   document.documentElement.style.setProperty('--modify', '135px')
   // }
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 700px)'
+  })
   return(
         <motion.div 
           className={'sidebar'}
           initial={false}
           animate={{
-            width: isOpen ? '135px' : '100px',
+            width: isDesktopOrLaptop? isOpen ? '135px' : '100px' : '100%',
+            marginTop: isDesktopOrLaptop? '40px': '0px',
+            height: isDesktopOrLaptop? '360px': '300px',
             // onAnimationEnd: end,
             transition: {
               duration: 0.3,
@@ -35,25 +41,24 @@ export default function General({currentPage}: {readonly currentPage:string}){
         <a className={currentPage==='/pg1'? "active" : ' '} href="#/pg1">Page 1</a>
         <a className={currentPage==='/pg2'? "active" : ' '} href="#/pg2">Page 2</a>
         <a className={currentPage==='/pg3'? "active" : ' '} href="#/pg3">Page 3</a>
-        <Nav>
-            <NavDropdown
-              id="nav-dropdown-dark-example"
-              title="More"
-              menuVariant="dark"
-              drop ="end"
-            >
-              <NavDropdown.Item href="#action/3.1">Eat</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Drink
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Sleep</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <button //Need change later
+        <NavDropdown
+          id="nav-dropdown"
+          title="More"
+          menuVariant="dark"
+          drop={isDesktopOrLaptop ? "end" : 'down-centered'}
+        >
+          <NavDropdown.Item href="#action/3.1">Eat</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">
+            Drink
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Sleep</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">
+            Separated link
+          </NavDropdown.Item>
+        </NavDropdown>
+          
+          {isDesktopOrLaptop && <button //Need change later
                   onClick={() => setIsOpen(!isOpen)}>
             <motion.div 
             className={'button'}
@@ -66,7 +71,71 @@ export default function General({currentPage}: {readonly currentPage:string}){
               }}
             >
             Control</motion.div>
-        </button>
+        </button>}
+        
       </motion.div>  
   )
 }
+// Fancy version
+// export const General:React.FC<GeneralProps>=({currentPage}: {readonly currentPage:string})=>{ 
+  //   document.body.classList.add('front');
+  //   const [isOpen, setIsOpen] = useState(false);
+  //   // if(!isOpen){
+  //   //   document.documentElement.style.setProperty('--modify', '100px')
+      
+  //   // }else{
+  //   //   document.documentElement.style.setProperty('--modify', '135px')
+  //   // }
+  //   return(
+  //         <motion.div 
+  //           className={'sidebar'}
+  //           initial={false}
+  //           animate={{
+  //             width: isOpen ? '135px' : '100px',
+  //             // onAnimationEnd: end,
+  //             transition: {
+  //               duration: 0.3,
+  //             },
+  //           }}
+  //         >
+            
+  //         <p></p>
+  //         <a className={currentPage==='/'? 'active' : ' '} href="/">Home</a>
+  //         <a className={currentPage==='/pg1'? "active" : ' '} href="#/pg1">Page 1</a>
+  //         <a className={currentPage==='/pg2'? "active" : ' '} href="#/pg2">Page 2</a>
+  //         <a className={currentPage==='/pg3'? "active" : ' '} href="#/pg3">Page 3</a>
+  //         <Nav>
+  //             <NavDropdown
+  //               id="nav-dropdown-dark-example"
+  //               title="More"
+  //               menuVariant="dark"
+  //               drop ="end"
+  //             >
+  //               <NavDropdown.Item href="#action/3.1">Eat</NavDropdown.Item>
+  //               <NavDropdown.Item href="#action/3.2">
+  //                 Drink
+  //               </NavDropdown.Item>
+  //               <NavDropdown.Item href="#action/3.3">Sleep</NavDropdown.Item>
+  //               <NavDropdown.Divider />
+  //               <NavDropdown.Item href="#action/3.4">
+  //                 Separated link
+  //               </NavDropdown.Item>
+  //             </NavDropdown>
+  //           </Nav>
+  //           <button //Need change later
+  //                   onClick={() => setIsOpen(!isOpen)}>
+  //             <motion.div 
+  //             className={'button'}
+  //             initial={false}
+  //             animate={{
+  //               width: isOpen ? '135px' : '100px',
+  //               transition: {
+  //                 duration: 0.3,
+  //               },
+  //               }}
+  //             >
+  //             Control</motion.div>
+  //         </button>
+  //       </motion.div>  
+  //   )
+  // }
--- src/home.tsx
+++ src/home.tsx
@@ -1,76 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';  
-import {Nav, NavDropdown, Container} from 'react-bootstrap';  
+import {Container} from 'react-bootstrap';  
 import './decor/helper.css';
-import { useState } from 'react';
-import { motion} from "framer-motion";
-
+import General from './general';
 
 export interface GeneralProps {
   currentPage: string,
 }
 //Fancy constant, but how good is it?
-export const General:React.FC<GeneralProps>=({currentPage}: {readonly currentPage:string})=>{
-  document.body.classList.add('front');
-  const [isOpen, setIsOpen] = useState(false);
-  // if(!isOpen){
-  //   document.documentElement.style.setProperty('--modify', '100px')
-    
-  // }else{
-  //   document.documentElement.style.setProperty('--modify', '135px')
-  // }
-  return(
-        <motion.div 
-          className={'sidebar'}
-          initial={false}
-          animate={{
-            width: isOpen ? '135px' : '100px',
-            // onAnimationEnd: end,
-            transition: {
-              duration: 0.3,
-            },
-          }}
-        >
-          
-        <p></p>
-        <a className={currentPage==='/'? 'active' : ' '} href="/">Home</a>
-        <a className={currentPage==='/pg1'? "active" : ' '} href="#/pg1">Page 1</a>
-        <a className={currentPage==='/pg2'? "active" : ' '} href="#/pg2">Page 2</a>
-        <a className={currentPage==='/pg3'? "active" : ' '} href="#/pg3">Page 3</a>
-        <Nav>
-            <NavDropdown
-              id="nav-dropdown-dark-example"
-              title="More"
-              menuVariant="dark"
-              drop ="end"
-            >
-              <NavDropdown.Item href="#action/3.1">Eat</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Drink
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Sleep</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <button //Need change later
-                  onClick={() => setIsOpen(!isOpen)}>
-            <motion.div 
-            className={'button'}
-            initial={false}
-            animate={{
-              width: isOpen ? '135px' : '100px',
-              transition: {
-                duration: 0.3,
-              },
-              }}
-            >
-            Control</motion.div>
-        </button>
-      </motion.div>  
-  )
-}
+// 
 function Home() {  
   
   return (  
